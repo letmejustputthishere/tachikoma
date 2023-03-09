3ea838d2506d0e8f7b56161243cfb59f8d34f936
Moritz Fuller <moritz.fuller@dfinity.org>
2023-03-09 08:36:42 +0100

    ✨ send tweet

--- workspaces/proxy/src/proxy.ts
+++ workspaces/proxy/src/proxy.ts
@@ -1,5 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
+import "dotenv/config";
+import TwitterApi from "twitter-api-v2";
+
+// create a new oauth 1.0a authenticated twitter client
+// make sure to have your .env file in the root of this project
+const client = new TwitterApi({
+    appKey: process.env.CONSUMER_KEY!,
+    appSecret: process.env.CONSUMER_SECRET!,
+    accessToken: process.env.ACCESS_TOKEN!,
+    accessSecret: process.env.ACCESS_TOKEN_SECRET!,
+}).readWrite;
 
 const app = express();
 const port = 3000;
@@ -7,10 +18,12 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+    // using the authenticated client, we can send a tweet on behalf of the user
+    client.v2.tweet("hello world!");
+    res.send("Tweet sent!");
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}.`);
+    console.log(`Server listening on port ${port}.`);
 });
